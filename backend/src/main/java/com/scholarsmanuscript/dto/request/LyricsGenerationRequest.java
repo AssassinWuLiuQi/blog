99e3e8e3c51b5da83076d7c217118194a752b746
MxJin <2806987234@qq.com>
2026-04-14 15:57:18 +0800

    fix(music): add missing Lombok annotations and @Builder.Default to request DTOs

--- backend/src/main/java/com/scholarsmanuscript/dto/request/LyricsGenerationRequest.java
+++ backend/src/main/java/com/scholarsmanuscript/dto/request/LyricsGenerationRequest.java
@@ -1,13 +1,20 @@
 package com.scholarsmanuscript.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LyricsGenerationRequest {
 
     @NotBlank(message = "prompt is required")
     private String prompt;
 
+    @Builder.Default
     private String mode = "write_full_song";
 }
--- backend/src/main/java/com/scholarsmanuscript/dto/request/MusicGenerationRequest.java
+++ backend/src/main/java/com/scholarsmanuscript/dto/request/MusicGenerationRequest.java
@@ -1,11 +1,18 @@
 package com.scholarsmanuscript.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MusicGenerationRequest {
 
+    @Builder.Default
     private String model = "music-2.6";
 
     @NotBlank(message = "prompt is required")
@@ -13,5 +20,6 @@ public class MusicGenerationRequest {
 
     private String lyrics;
 
+    @Builder.Default
     private boolean isInstrumental = false;
 }
